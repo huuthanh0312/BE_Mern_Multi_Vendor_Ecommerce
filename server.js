d418e5be7a98580ab96190aa663a152ecbbb2fca
huuthanh0312 <huuthanhnguyen0312@gmail.com>
2024-10-10 13:01:31 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+//bodyParser
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+app.use('/api', require('./routes/authRoutes'))
+app.get('/', (req, res) => res.send('My Backend'))
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
